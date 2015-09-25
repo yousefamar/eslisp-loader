aceb6358800e9d4ac04fdf19352882e71ded4f80
Yousef Amar <yousefamar@gmail.com>
2015-09-25 13:40:56 +0200

    Make eslisp a peer dependency

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var eslisp = require("eslisp");
 module.exports = function (source) {
 	this.cacheable && this.cacheable();
 
-	var eslispRequest = loaderUtils.getRemainingRequest(this);
-	var jsRequest = loaderUtils.getCurrentRequest(this);
+	//var eslispRequest = loaderUtils.getRemainingRequest(this);
+	//var jsRequest = loaderUtils.getCurrentRequest(this);
 	var query = loaderUtils.parseQuery(this.query);
+
+	query.transformMacros = this.eslisp.tranforms.map(require);
 
 	var result = eslisp(source, query);
 
